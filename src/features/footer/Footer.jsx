7702fa0c87ffe5451f9f0b6675542a74423d8f01
Albert Johanson <aj.dev.gt@gmail.com>
2021-02-26 23:43:25 -0600

    Adding style to the links in the footer

---
+++ src/features/footer/Footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+//assets
+import logo from "./assets/logo.svg";
+
+//style
+import "./sass/Footer.scss";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer__logo">
+        <figure>
+          <img src={logo} alt="" />
+        </figure>
+      </div>
+      <div className="footer__options">
+        <div className="options" style={{borderLeft: "solid 5px #6B407E"}}>
+          <a href="/">Informacion de envíos</a>
+        </div>
+        <div className="options" style={{borderLeft: "solid 5px #29B8E8"}}>
+          <a href="/">Política de devoluciones</a>
+        </div>
+        <div className="options" style={{borderLeft: "solid 5px #FA7800"}}>
+          <a href="/">Métodos de pago</a>
+        </div>
+        <div className="options" style={{borderLeft: "solid 5px #CB161D"}}>
+          <a href="/">Política de privacidad</a>
+        </div>
+        <div className="options" style={{borderLeft: "solid 5px #33B13A"}}>
+          <a href="/">Términos y Condiciones</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
